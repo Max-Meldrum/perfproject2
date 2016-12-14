017e16be8d79ac4c6fbabc5aa569488798ec0b64
johan-bjareholt <johanthepro@gmail.com>
2016-12-14 15:26:11 +0100

    Fancy pants benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ function run(){
     prog=$1
     iters=$2
     t=$({ time $(echo $iters | $prog >/dev/null); } 2>&1 | grep real | sed -e 's/.*m\([0-9]\+\.[0-9]\+\)s/\1/')
-    echo -e "$prog\t$t"
+    printf "%s" $t
 }
 
 # Warmup processor
@@ -15,5 +15,16 @@ function run(){
 
 # GO!
 echo "Starting benchmark"
+printf "Fractal sequential: "
 run ./fractal_seq
+printf "\n"
+
+THREAD_COUNT=1 make fractal_par > /dev/null
+printf "Fractal parallell(1): "
 run ./fractal_par
+printf "\n"
+
+THREAD_COUNT=8 make fractal_par > /dev/null
+printf "Fractal parallell(8): "
+run ./fractal_par
+printf "\n"
